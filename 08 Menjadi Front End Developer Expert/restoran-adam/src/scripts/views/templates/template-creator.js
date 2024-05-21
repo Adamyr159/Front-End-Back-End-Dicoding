47488835eb7d977ebc2eb8e912b9d14517bbe4c6
Adamyr159 <adamyr159@Gmail.com>
2024-05-21 21:54:38 +0700

    Update halaman detail restoran

--- 08 Menjadi Front End Developer Expert/restoran-adam/src/scripts/views/templates/template-creator.js
+++ 08 Menjadi Front End Developer Expert/restoran-adam/src/scripts/views/templates/template-creator.js
@@ -1,34 +1,61 @@
-import CONFIG from '../../globals/config';
- 
+import CONFIG from "../../globals/config";
+
 const createRestaurantDetailTemplate = (restaurant) => `
-  <h2 class="restaurant__title">${restaurant.name}</h2>
-  <img class="restaurant__poster" src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}" alt="${restaurant.name}" />
-  <div class="restaurant__info">
-    <h3>Information</h3>
-    <h4>city</h4>
-    <p>${restaurant.city}</p>
-    <h4>Rating</h4>
-    <p>${restaurant.rating}</p>
-    <h4>Kategori</h4>
-    <p>${restaurant.categories.map((categories) => categories.name).join(",")}</p>
-    <h4>Menu Makanan</h4>
-    <p>${restaurant.menus.foods.map((food) => food.name).join(",")}</p>
-    <h4>Menu Minuman</h4>
-    <p>${restaurant.menus.drinks.map((drink) => drink.name).join(",")}</p>
+  <div class="container">
+    <div class="row">
+        <div class="column image-column">
+            <img src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}" alt="${restaurant.name}" class="restaurant-image">
+        </div>
+        <div class="column details-column">
+            <h1 class="resto-name">${restaurant.name}</h1>
+            <p class="address"><strong>Alamat:</strong> ${restaurant.address}</p>
+            <p class="city"><strong>Kota:</strong> ${restaurant.city}</p>
+            <p class="description">${restaurant.description}</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="column menu-column">
+            <h2>Menu Makanan</h2>
+            ${restaurant.menus.foods.map((food) => `
+              <ul class="menu-list">
+                  <li class="menu-item">${ food.name }</li>
+              </ul>
+            `).join("")}
+        </div>
+        <div class="column menu-column">
+            <h2>Menu Minuman</h2>
+            ${restaurant.menus.drinks.map((drink) => `
+              <ul class="menu-list">
+                  <li class="menu-item">${ drink.name }</li>
+              </ul>
+            `).join("")}
+        </div>
+    </div>
+    <div class="row">
+      <h2>Pendapat Mereka</h2>
+        <div class="column review-column">
+            ${restaurant.customerReviews.map((cust) => `
+            <div class="review-card">
+                <h5 class="review-title">${cust.name} ⭐️5</h5>
+                <p class="review-text">${cust.review}</p>
+                <p class="review-date">${cust.date}</p>
+            </div>
+            `).join("")}
+        </div>
+    </div>
   </div>
-  <div class="restaurant__overview">
-    <h3>overview</h3>
-    <p>${restaurant.description}</p>
-  </div>
-`;
- 
+  
+    `;
+// jangan lupa masukin review customer di createRestaurantDetailTemplate
 const createRestaurantItemTemplate = (restaurant) => `
   <div class="restaurant-item">
     <div class="restaurant-item__header">
       <img class="restaurant-item__header__poster" alt="${restaurant.name}"
            src="${CONFIG.BASE_IMAGE_URL + restaurant.pictureId}">
       <div class="restaurant-item__header__rating">
-        <p>⭐️<span class="restaurant-item__header__rating__score">${restaurant.rating}</span></p>
+        <p>⭐️<span class="restaurant-item__header__rating__score">${
+          restaurant.rating
+        }</span></p>
       </div>
     </div>
     <div class="restaurant-item__content">
