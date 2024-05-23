1013a344b405790b1fbda72d76108a2fae0ecc6e
Adamyr159 <adamyr159@Gmail.com>
2024-05-23 11:15:13 +0700

    Add button like & save in indexDB

--- 08 Menjadi Front End Developer Expert/restoran-adam/src/scripts/globals/config.js
+++ 08 Menjadi Front End Developer Expert/restoran-adam/src/scripts/globals/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
     DEFAULT_LANGUAGE: 'en-us',
     // CACHE_NAME: new Date().toISOString(),
-    DATABASE_NAME: 'restaurants-database',
+    DATABASE_NAME: 'restaurant-database',
     DATABASE_VERSION: 1,
     OBJECT_STORE_NAME: 'restaurants',
     // WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev', // web socket
--- 08 Menjadi Front End Developer Expert/restoran-adam/src/scripts/views/pages/detail.js
+++ 08 Menjadi Front End Developer Expert/restoran-adam/src/scripts/views/pages/detail.js
@@ -1,19 +1,32 @@
 import UrlParser from "../../routes/url-parser";
 import Restaurant from "../../data/restaurant-source";
-import { createRestaurantDetailTemplate } from '../templates/template-creator';
+import { createRestaurantDetailTemplate } from "../templates/template-creator";
+import LikeButtonInitiator from "../../utils/like-button-initiator";
 
 const Detail = {
   async render() {
     return `
-      <div id="restaurant" class="restaurant"></div>
+    <div id="restaurant" class="restaurant"></div>
+    <div id="likeButtonContainer"></div>
     `;
   },
 
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const restaurant = await Restaurant.detailRestaurant(url.id);
-    const restaurantContainer = document.querySelector('#restaurant');
+    const restaurantContainer = document.querySelector("#restaurant");
     restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector("#likeButtonContainer"),
+      restaurant: {
+        id: restaurant.id,
+        title: restaurant.title,
+        overview: restaurant.overview,
+        backdrop_path: restaurant.backdrop_path,
+        vote_average: restaurant.vote_average,
+      },
+    });
   },
 };
 
