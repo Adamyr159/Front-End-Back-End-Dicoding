e5d6698cf2ecc26f40e449355164c6c4c96fe585
Adamyr159 <adamyr159@Gmail.com>
2024-06-05 19:33:43 +0700

    update e2e testing like restaurant

--- 08 Menjadi Front End Developer Expert/restoran-adam/e2e/Liking_Restaurant.test.js
+++ 08 Menjadi Front End Developer Expert/restoran-adam/e2e/Liking_Restaurant.test.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 Feature("Liking Restaurant");
 
 Before(({ I }) => {
@@ -10,10 +12,22 @@ Scenario("showing empty liked restaurants", ({ I }) => {
   I.see("Tidak ada restaurant yang ditampilkan", ".restaurant-item__not__found");
 });
 
-Scenario("liking one restaurant", ({ I }) => {
+Scenario("liking one restaurant", async ({ I }) => {
   I.see("Tidak ada restaurant yang ditampilkan", ".restaurant-item__not__found");
 
   I.amOnPage("/");
 
-  pause();
+  I.seeElement('.restaurant__title a');
+  const firstRestaurant = locate('.restaurant__title a').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('.restaurant-item');
+  const likedRestaurantTitle = await I.grabTextFrom('.restaurant__title');
+ 
+  assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
 });
