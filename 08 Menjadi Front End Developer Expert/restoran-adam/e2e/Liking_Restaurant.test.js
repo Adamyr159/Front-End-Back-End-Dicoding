83c64f031d37fed6269d1e779ab0c883e018463f
Adamyr159 <adamyr159@Gmail.com>
2024-06-05 16:42:33 +0700

    Fixing Bug: e2e testing

--- 08 Menjadi Front End Developer Expert/restoran-adam/e2e/Liking_Restaurant.test.js
+++ 08 Menjadi Front End Developer Expert/restoran-adam/e2e/Liking_Restaurant.test.js
@@ -7,13 +7,13 @@ Before(({ I }) => {
 Scenario("showing empty liked restaurants", ({ I }) => {
   I.wait(10);
   I.seeElement("#restaurants");
-  I.see("Tidak ada restaurant yang ditampilkan", ".restaurant-not-found");
+  I.see("Tidak ada restaurant yang ditampilkan", ".restaurant-item__not__found");
 });
 
-// Scenario("liking one restaurant", ({ I }) => {
-//   I.see("Tidak ada film untuk ditampilkan", ".restaurant-not-found");
+Scenario("liking one restaurant", ({ I }) => {
+  I.see("Tidak ada restaurant yang ditampilkan", ".restaurant-item__not__found");
 
-//   I.amOnPage("/");
+  I.amOnPage("/");
 
-//   pause();
-// });
+  pause();
+});
